3f0060f2d35c01e56adc5fd45b85b1b18bac5ed6
danrantino <danielaugustofurst@hotmail.com>
2026-08-25 14:10:33 +0000

    fix allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['web.domus.dev'],
+    allowedHosts: ['web.domus.dev','domus-front-preprod.up.railway.app','domus-front-prod.up.railway.app'],
   },
   preview: {
     host: '0.0.0.0',
